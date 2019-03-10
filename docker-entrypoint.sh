d7b1b5fe6de16279c629216af8df065de11fb057
Carlo Beltrame <carlo@beltra.me>
2019-03-10 14:50:09 +0100

    Configure Laravel for other developers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,9 @@
 
 composer install --ignore-platform-reqs --no-interaction --no-plugins --no-scripts --prefer-dist
 
+if [ ! -f .env ]; then
+    cp .env.example .env
+    php artisan key:generate
+fi
+
 apache2-foreground
