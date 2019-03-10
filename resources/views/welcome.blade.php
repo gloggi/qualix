d9c8004f36186e3ff456993886c4b0e66e407b72
Davud <x123xde@gmail.com>
2019-03-10 15:08:35 +0100

    Lindo is online

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Qualix
                 </div>
 
                 <div class="links">
