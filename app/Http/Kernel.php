22b1cb13b635ee75ba4eaae47229e16f87acb692
Carlo Beltrame <carlo@beltra.me>
2019-03-15 14:10:04 +0100

    Always run the CSRF middleware after the Authentication middleware, to make sure that the logout route is redirected to login without error when the CSRF token has expired

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -76,5 +76,6 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
+        \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 }
