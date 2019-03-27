859835c6bcd5fe66b636762c56751d7c9afdfc7e
Carlo Beltrame <carlo@beltra.me>
2019-03-27 14:27:54 +0100

    Clean up CourseController

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -10,36 +10,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 
-class CourseController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
+class CourseController extends Controller {
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
+    public function create() {
         return view('admin.newcourse');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  CourseStoreRequest  $request
+     * @param  CourseStoreRequest $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(CourseStoreRequest $request)
-    {
+    public function store(CourseStoreRequest $request) {
         $validatedData = $request->validated();
         $kurs = new Kurs($validatedData);
         $kurs->save();
@@ -56,8 +43,7 @@ class CourseController extends Controller
      * @param CourseSelectRequest $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function select(CourseSelectRequest $request)
-    {
+    public function select(CourseSelectRequest $request) {
         $validatedData = $request->validated();
         /** @var User $user */
         $user = Auth::user();
@@ -69,46 +55,23 @@ class CourseController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show()
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
+    public function update(Request $request, $id) {
         //
     }
 }
