ab4ee53ef7ab1f6b3144f65ed75a638e25ffb03c
Carlo Beltrame <carlo@beltra.me>
2019-04-09 17:17:52 +0200

    Add message when there are no QKs and MAs

--- resources/views/admin/ma.blade.php
+++ resources/views/admin/ma.blade.php
@@ -4,21 +4,29 @@
 
     @component('components.card', ['header' => __('Mindestanforderungen :courseName', ['courseName' => $kurs->name])])
 
-        @component('components.responsive-table', [
-            'data' => $kurs->mas,
-            'fields' => [
-                __('Anforderung') => function(\App\Models\MA $ma) { return $ma->anforderung; },
-                __('Killer') => function(\App\Models\MA $ma) { return $ma->killer ? __('Ja') : __('Nein'); },
-                __('Anzahl Beobachtungen') => function(\App\Models\MA $ma) { return count($ma->beobachtungen); },
-            ],
-            'actions' => [
-                'edit' => function(\App\Models\MA $ma) use ($kurs) { return route('admin.ma.edit', ['kurs' => $kurs->id, 'ma' => $ma->id]); },
-                'delete' => function(\App\Models\MA $ma) use ($kurs) { return [
-                    'text' => __('Willst du diese Mindestanforderung wirklich löschen? ' . count($ma->beobachtungen) . ' Beobachtung(en) ist / sind darauf zugewiesen.'),
-                    'route' => ['admin.ma.delete', ['kurs' => $kurs->id, 'ma' => $ma->id]],
-                 ];},
-            ]
-        ])@endcomponent
+        @if (count($kurs->mas))
+
+            @component('components.responsive-table', [
+                'data' => $kurs->mas,
+                'fields' => [
+                    __('Anforderung') => function(\App\Models\MA $ma) { return $ma->anforderung; },
+                    __('Killer') => function(\App\Models\MA $ma) { return $ma->killer ? __('Ja') : __('Nein'); },
+                    __('Anzahl Beobachtungen') => function(\App\Models\MA $ma) { return count($ma->beobachtungen); },
+                ],
+                'actions' => [
+                    'edit' => function(\App\Models\MA $ma) use ($kurs) { return route('admin.ma.edit', ['kurs' => $kurs->id, 'ma' => $ma->id]); },
+                    'delete' => function(\App\Models\MA $ma) use ($kurs) { return [
+                        'text' => __('Willst du diese Mindestanforderung wirklich löschen? ' . count($ma->beobachtungen) . ' Beobachtung(en) ist / sind darauf zugewiesen.'),
+                        'route' => ['admin.ma.delete', ['kurs' => $kurs->id, 'ma' => $ma->id]],
+                     ];},
+                ]
+            ])@endcomponent
+
+        @else
+
+            {{__('Bisher sind keine Mindestanforderungen erfasst.')}}
+
+        @endif
 
     @endcomponent
 
