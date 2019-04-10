20dd4756f461f41fc48188e93ef94d858bc9587a
Carlo Beltrame <carlo@beltra.me>
2019-04-10 14:19:02 +0200

    Test the message when no QK present

--- tests/Feature/CreateQKTest.php
+++ tests/Feature/CreateQKTest.php
@@ -42,7 +42,7 @@ class CreateQKTest extends TestCaseWithKurs {
         $response->assertSee($this->payload['quali_kategorie']);
     }
 
-    public function test_shouldValidateNewQKData_noQualiKategorieName() {
+    public function test_shouldValidateNewQKData_noQualikategorieName() {
         // given
         $payload = $this->payload;
         unset($payload['quali_kategorie']);
@@ -52,5 +52,28 @@ class CreateQKTest extends TestCaseWithKurs {
 
         // then
         $this->assertInstanceOf(ValidationException::class, $response->exception);
+    }
+
+    public function test_shouldShowMessage_whenNoQKInCourse() {
+        // given
+
+        // when
+        $response = $this->get('/kurs/' . $this->kursId . '/admin/qk', $this->payload);
+
+        // then
+        $response->assertStatus(200);
+        $response->assertSee('Bisher sind keine Qualikategorien erfasst.');
+    }
+
+    public function test_shouldNotShowMessage_whenSomeQKInCourse() {
+        // given
+        $this->post('/kurs/' . $this->kursId . '/admin/qk', $this->payload);
+
+        // when
+        $response = $this->get('/kurs/' . $this->kursId . '/admin/qk', $this->payload);
+
+        // then
+        $response->assertStatus(200);
+        $response->assertDontSee('Bisher sind keine Qualikategorien erfasst.');
     }
 }
