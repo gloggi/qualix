b6cfc89ce34189c2d78b08bcc504689ce4635af0
Carlo Beltrame <carlo@beltra.me>
2019-04-11 18:02:18 +0200

    Add tests for deleting blocks

---
+++ tests/Feature/Admin/Bloecke/DeleteBlockTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\TestResponse;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCaseWithKurs;
+
+class DeleteBlockTest extends TestCaseWithKurs {
+
+    private $blockId;
+
+    public function setUp(): void {
+        parent::setUp();
+
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.1', 'blockname' => 'Block 1', 'datum' => '01.01.2019']);
+        /** @var User $user */
+        $user = Auth::user();
+        $this->blockId = $user->lastAccessedKurs->bloecke()->first()->id;
+    }
+
+    public function test_shouldRequireLogin() {
+        // given
+        auth()->logout();
+
+        // when
+        $response = $this->delete('/kurs/' . $this->kursId . '/admin/bloecke/' . $this->blockId);
+
+        // then
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_shouldDeleteQK() {
+        // given
+
+        // when
+        $response = $this->delete('/kurs/' . $this->kursId . '/admin/bloecke/' . $this->blockId);
+
+        // then
+        $response->assertStatus(302);
+        $response->assertRedirect('/kurs/' . $this->kursId . '/admin/bloecke');
+        /** @var TestResponse $response */
+        $response = $response->followRedirects();
+        $response->assertDontSee('Qualikategorie 1');
+    }
+
+    public function test_shouldValidateDeletedQKUrl_wrongId() {
+        // given
+
+        // when
+        $response = $this->delete('/kurs/' . $this->kursId . '/admin/bloecke/' . ($this->blockId + 1));
+
+        // then
+        $response->assertStatus(404);
+    }
+}
