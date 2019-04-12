169a64154edaeca76694ff090d2f93644f969a5e
Carlo Beltrame <carlo@beltra.me>
2019-04-12 15:11:39 +0200

    Extract composer install into separate container, and improve performance of docker containers on Windows and Mac by optimizing mount / volume structures

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-composer install --ignore-platform-reqs --no-interaction --no-plugins --no-scripts --prefer-dist
 
 if [[ ! -f .env ]]; then
     cp .env.example .env
@@ -11,8 +10,10 @@ if [[ ! -f .env ]]; then
     php artisan config:cache
 fi
 
+mkdir -p storage/app/public storage/framework/cache/data storage/framework/sessions storage/framework/testing/disks/local storage/framework/views
 chmod 777 -R storage bootstrap/cache
 
+php bin/wait-for-composer-install.php
 php artisan db:wait
 php artisan migrate
 
