fe768cf63143d23d1861224d628da8809afd6966
Carlo Beltrame <carlo@beltra.me>
2019-05-14 13:11:38 +0200

    Add test that checks the ordering of observations on TN detail page

--- tests/Feature/Beobachtung/ReadBeobachtungTest.php
+++ tests/Feature/Beobachtung/ReadBeobachtungTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\Beobachtung;
 
+use App\Models\Block;
 use App\Models\Kurs;
 use App\Models\User;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Tests\TestCaseWithBasicData;
 
@@ -69,5 +71,43 @@ class ReadBeobachtungTest extends TestCaseWithBasicData {
 
         // then
         $this->assertInstanceOf(ModelNotFoundException::class, $response->exception);
+    }
+
+    public function test_shouldOrderBeobachtungenByBlockOrder() {
+        // given
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.1', 'blockname' => 'later date', 'datum' => '02.01.2019', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.1', 'blockname' => 'earlier date', 'datum' => '31.12.2018', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '2.1', 'blockname' => 'later day number', 'datum' => '01.01.2019', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '0.1', 'blockname' => 'earlier day number', 'datum' => '01.01.2019', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.2', 'blockname' => 'later block number', 'datum' => '01.01.2019', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.0', 'blockname' => 'earlier block number', 'datum' => '01.01.2019', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.1', 'blockname' => 'Block 2 later block name', 'datum' => '01.01.2019', 'ma_ids' => null]);
+        $this->post('/kurs/' . $this->kursId . '/admin/bloecke', ['full_block_number' => '1.1', 'blockname' => 'Block 0 earlier block name', 'datum' => '01.01.2019', 'ma_ids' => null]);
+        /** @var User $user */
+        $user = Auth::user();
+        /** @var Collection $blockIds */
+        $blockIds = $user->lastAccessedKurs->bloecke->map(function (Block $block) { return $block->id; });
+        $blockIdsToCreateBeobachtungen = $blockIds->sort();
+        $blockIdsToCreateBeobachtungen->shift();
+        foreach ($blockIdsToCreateBeobachtungen as $blockId) {
+            $this->post('/kurs/' . $this->kursId . '/beobachtungen/neu', ['tn_ids' => '' . $this->tnId, 'kommentar' => Block::find($blockId)->blockname, 'bewertung' => '1', 'block_id' => '' . $blockId, 'ma_ids' => '', 'qk_ids' => '']);
+        }
+
+        // when
+        $response = $this->get('/kurs/' . $this->kursId . '/tn/' . $this->tnId);
+
+        // then
+        $response->assertOk();
+        $this->assertSeeAllInOrder('table.table-responsive-cards td[data-label^="Beobachtung"]', [
+          'earlier date',
+          'earlier day number',
+          'earlier block number',
+          'Block 0 earlier block name',
+          'hat gut mitgemacht',
+          'Block 2 later block name',
+          'later block number',
+          'later day number',
+          'later date',
+        ]);
     }
 }
