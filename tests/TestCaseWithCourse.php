c909c545ddff785e9d62ce474164608cfaccdf44
Carlo Beltrame <carlo@beltra.me>
2019-06-17 08:53:29 +0200

    Rename variable to english

--- tests/TestCaseWithCourse.php
+++ tests/TestCaseWithCourse.php
@@ -21,8 +21,10 @@ abstract class TestCaseWithCourse extends TestCase
         return Participant::create(['course_id' => ($courseId !== null ? $courseId : $this->courseId), 'scout_name' => $scoutName])->id;
     }
 
-    protected function createBlock($blockname = 'Block 1', $fullBlockNumber = '1.1', $date = '01.01.2019', $maIds = null, $courseId = null) {
-        return Block::create(['course_id' => ($courseId !== null ? $courseId : $this->courseId), 'full_block_number' => $fullBlockNumber, 'name' => $blockname, 'block_date' => $date, 'requirement_ids' => $maIds])->id;
+    protected function createBlock($blockname = 'Block 1', $fullBlockNumber = '1.1', $date = '01.01.2019', $requirementIds = null, $courseId = null) {
+        $block = Block::create(['course_id' => ($courseId !== null ? $courseId : $this->courseId), 'full_block_number' => $fullBlockNumber, 'name' => $blockname, 'block_date' => $date]);
+        $block->requirements()->attach($requirementIds);
+        return $block->id;
     }
 
     protected function createCategory($name = 'Kategorie 1', $courseId = null) {
