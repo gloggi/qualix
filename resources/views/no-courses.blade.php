46dc0b0f433e27c2a24bdb250fb068bb735d0857
Carlo Beltrame <carlo@beltra.me>
2019-09-11 13:18:03 +0200

    Improve welcome message now that registering / login is very easy

--- resources/views/no-courses.blade.php
+++ resources/views/no-courses.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    @component('components.card', ['header' => __('Willkommen')])
+    @component('components.card', ['header' => __('Willkommen bei Qualix!')])
 
         {{ __('Du bist momentan noch in keinem Kurs eingetragen. Lass dich in einen Kurs einladen oder erstelle selber einen neuen.') }}
 
