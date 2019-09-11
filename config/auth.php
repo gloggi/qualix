7cf2b299a9975044b35eb4f000ce9a7b25db068e
Carlo Beltrame <carlo@beltra.me>
2019-09-11 13:14:00 +0200

    Fix config for hitobito login

--- config/auth.php
+++ config/auth.php
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'nativeUsers',
+            'provider' => 'users',
         ],
 
         'api' => [
