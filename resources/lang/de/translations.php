37b6337815de466078f9a7007ba4f67eb8051729
Carlo Beltrame <carlo@beltra.me>
2019-11-11 17:44:37 +0100

    Rename translations files

---
+++ resources/lang/de/translations.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'auth' => [
+        'failed' => 'Dieses Login ist uns nicht bekannt. Meldest du dich vielleicht normalerweise mit MiData an?',
+        'throttle' => 'Zu viele Login-Versuche. Bitte probiere es nochmals in :seconds Sekunden.',
+    ],
+];
