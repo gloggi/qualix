6547e6bcc004b7edafeb584056274f8a5b016158
Carlo Beltrame <carlo@beltra.me>
2019-11-11 17:54:03 +0100

    Updated translations from Phrase
    Updated locale file translations.php
    Updated locale file translations.php

--- resources/lang/de/translations.php
+++ resources/lang/de/translations.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'auth' => [
-        'failed' => 'Dieses Login ist uns nicht bekannt. Meldest du dich vielleicht normalerweise mit MiData an?',
-        'throttle' => 'Zu viele Login-Versuche. Bitte probiere es nochmals in :seconds Sekunden.',
-    ],
-];
+return array(
+	"auth" => array(
+		"failed" => "Dieses Login ist uns nicht bekannt. Meldest du dich vielleicht normalerweise mit MiData an?",
+		"throttle" => "Zu viele Login-Versuche. Bitte probiere es nochmals in :seconds Sekunden.",
+	),
+);
--- resources/lang/fr/translations.php
+++ resources/lang/fr/translations.php
@@ -1,4 +1,7 @@
 <?php
-
-return [
-];
+return array(
+	"auth" => array(
+		"failed" => "Ces identifiants ne correspondent pas à nos enregistrements. Est-ce que tu enregistre avec MiData normalement?",
+		"throttle" => "Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.",
+	),
+);
