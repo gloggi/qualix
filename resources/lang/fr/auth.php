3fcc35aa0e0ff978b3776e4ca8f7fa8f3aac11a8
Carlo Beltrame <carlo@beltra.me>
2019-11-17 00:05:20 +0100

    Updated translations from Phrase
    Updated locale file auth.php
    Updated locale file auth.php
    Updated locale file pagination.php
    Updated locale file pagination.php
    Updated locale file passwords.php
    Updated locale file passwords.php
    Updated locale file t.php
    Updated locale file t.php
    Updated locale file validation.php
    Updated locale file validation.php

--- resources/lang/fr/auth.php
+++ resources/lang/fr/auth.php
@@ -1,19 +1,5 @@
 <?php
-
-return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Authentication Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
-    | these language lines according to your application's requirements.
-    |
-    */
-
-    "failed" => "Ces identifiants ne correspondent pas à nos enregistrements. Est-ce que tu enregistre avec MiData normalement?",
-    "throttle" => "Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.",
-
-];
+return array(
+	"failed" => "Ces identifiants ne correspondent pas à nos enregistrements. Est-ce que tu enregistre avec MiData normalement?",
+	"throttle" => "Tentatives de connexion trop nombreuses. Veuillez essayer de nouveau dans :seconds secondes.",
+);
