bd3f8ea89616e57c8e2a5ac880e0833f1d520aa0
Carlo Beltrame <carlo@beltra.me>
2019-11-19 16:03:04 +0100

    Localize header, fixes #51

--- resources/lang/de/t.php
+++ resources/lang/de/t.php
@@ -4,6 +4,46 @@ return array(
         'slogan' => 'Qualix. was gaffsch?',
     ),
     'header' => array(
+        'course_admin' => 'Kursadmin',
         'language_switch' => 'Sprache wechseln. Übersetzungen via Phrase.com',
-    )
+        'qualix' => 'Qualix',
+        'welcome' => 'Willkommä, :user',
+    ),
+    'views' => array(
+        'admin' => array(
+            'course_settings' => array(
+                'title' => 'Kurseinstellungen',
+            ),
+            'equipe' => array(
+                'title' => 'Equipe',
+            ),
+            'requirements' => array(
+                'title' => 'Mindestanforderungen',
+            ),
+            'categories' => array(
+                'title' => 'Kategorien',
+            ),
+            'blocks' => array(
+                'title' => 'Blöcke',
+            ),
+            'participants' => array(
+                'title' => 'TN',
+            ),
+            'new_course' => array(
+                'title' => 'Neuen Kurs erstellen',
+            ),
+        ),
+        'blocks' => array(
+            'title' => 'Blöcke',
+        ),
+        'participants' => array(
+            'title' => 'TN',
+        ),
+        'overview' => array(
+            'title' => 'Überblick',
+        ),
+        'crib' => array(
+            'title' => 'Spick',
+        ),
+    ),
 );
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,5 @@
 <nav class="container navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/"><img class="avatar-small" src="{{ asset('images/was-gaffsch.svg') }}" />Qualix</a>
+    <a class="navbar-brand" href="/"><img class="avatar-small" src="{{ asset('images/was-gaffsch.svg') }}" />@lang('t.header.qualix')</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -14,7 +14,7 @@
                                 <option value="{{ route('index', ['course' => $c->id]) }}"{{ $course->id === $c->id ? ' selected' : '' }}>{{ $c->name }}</option>
                             @endforeach
                             @if(Auth::user()->archivedCourses()->count())
-                                <optgroup label="{{__('Archiviert')}}">
+                                <optgroup label="@lang('t.header.course.archived')">
                                     @foreach(Auth::user()->archivedCourses as $c)
                                         <option value="{{ route('index', ['course' => $c->id]) }}"{{ $course->id === $c->id ? ' selected' : '' }}>{{ $c->name }}</option>
                                     @endforeach
@@ -23,45 +23,45 @@
                         </select>
                     </li>
                     <li class="nav-item{{ Route::currentRouteName() == 'blocks' ? ' active' : '' }}">
-                        <a class="nav-link" href="{{ route('blocks', ['course' => $course->id]) }}">Blöcke</a>
+                        <a class="nav-link" href="{{ route('blocks', ['course' => $course->id]) }}">@lang('t.views.blocks.title')</a>
                     </li>
 
                     @if(!$course->archived)
                         <li class="nav-item{{ Route::currentRouteName() == 'participants' ? ' active' : '' }}">
-                            <a class="nav-link" href="{{ route('participants', ['course' => $course->id]) }}">TN</a>
+                            <a class="nav-link" href="{{ route('participants', ['course' => $course->id]) }}">@lang('t.views.participants.title')</a>
                         </li>
                         <li class="nav-item{{ Route::currentRouteName() == 'overview' ? ' active' : '' }}">
-                            <a class="nav-link" href="{{ route('overview', ['course' => $course->id]) }}">Überblick</a>
+                            <a class="nav-link" href="{{ route('overview', ['course' => $course->id]) }}">@lang('t.views.overview.title')</a>
                         </li>
                     @endif
                     <li class="nav-item{{ Route::currentRouteName() == 'crib' ? ' active' : '' }}">
-                        <a class="nav-link" href="{{ route('crib', ['course' => $course->id]) }}">Spick</a>
+                        <a class="nav-link" href="{{ route('crib', ['course' => $course->id]) }}">@lang('t.views.crib.title')</a>
                     </li>
                     <li class="nav-item dropdown{{ substr( Route::currentRouteName(), 0, 5 ) == 'admin' ? ' active' : '' }}">
                         <a class="nav-link dropdown-toggle" id="navbarCourseAdmin" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Kursadmin
+                            @lang('t.header.course_admin')
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarCourseAdmin">
                             <a class="dropdown-item{{ Route::currentRouteName() == 'admin.course' ? ' active' : '' }}"
-                               href="{{ route('admin.course', ['course' => $course->id]) }}">{{__('Kurseinstellungen')}}</a>
+                               href="{{ route('admin.course', ['course' => $course->id]) }}">@lang('t.views.admin.course_settings.title')</a>
                             <a class="dropdown-item{{ Route::currentRouteName() == 'admin.equipe' ? ' active' : '' }}"
-                               href="{{ route('admin.equipe', ['course' => $course->id]) }}">Equipe</a>
+                               href="{{ route('admin.equipe', ['course' => $course->id]) }}">@lang('t.views.admin.equipe.title')</a>
                             <a class="dropdown-item{{ Route::currentRouteName() == 'admin.requirements' ? ' active' : '' }}"
-                               href="{{ route('admin.requirements', ['course' => $course->id]) }}">Mindestanforderungen</a>
+                               href="{{ route('admin.requirements', ['course' => $course->id]) }}">@lang('t.views.admin.requirements.title')</a>
                             <a class="dropdown-item{{ Route::currentRouteName() == 'admin.categories' ? ' active' : '' }}"
-                               href="{{ route('admin.categories', ['course' => $course->id]) }}">Kategorien</a>
+                               href="{{ route('admin.categories', ['course' => $course->id]) }}">@lang('t.views.admin.categories.title')</a>
                             <a class="dropdown-item{{ Route::currentRouteName() == 'admin.blocks' ? ' active' : '' }}"
-                               href="{{ route('admin.blocks', ['course' => $course->id]) }}">Blöcke</a>
+                               href="{{ route('admin.blocks', ['course' => $course->id]) }}">@lang('t.views.admin.blocks.title')</a>
                             @if(!$course->archived)
                                 <a class="dropdown-item{{ Route::currentRouteName() == 'admin.participants' ? ' active' : '' }}"
-                                   href="{{ route('admin.participants', ['course' => $course->id]) }}">TN</a>
+                                   href="{{ route('admin.participants', ['course' => $course->id]) }}">@lang('t.views.admin.participants.title')</a>
                             @endif
                         </div>
                     </li>
                 @endif
                 <li class="nav-item{{ Route::currentRouteName() == 'admin.newcourse' ? ' active' : '' }}">
-                    <a class="nav-link" href="{{ route('admin.newcourse') }}">Neuen Kurs erstellen</a>
+                    <a class="nav-link" href="{{ route('admin.newcourse') }}">@lang('t.views.admin.new_course.title')</a>
                 </li>
             </ul>
         @endauth
@@ -81,14 +81,14 @@
             @auth
                 <li class="nav-item{{ Route::currentRouteName() == 'benutzer' ? ' active' : '' }}">
                     <a class="nav-link" href="{{ route('user') }}">
-                        Willkommä, {{ Auth::user()->name }}
+                        @lang('t.header.welcome', [ 'user' => Auth::user()->name ])
                         <img class="avatar-small" src="{{ Auth::user()->image_url != null ? asset(Storage::url(Auth::user()->image_url)) : asset('images/was-gaffsch.svg') }}">
                     </a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('logout') }}"
                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                        Uslogge
+                        @lang('Logout')
                     </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
@@ -96,10 +96,10 @@
                 </li>
             @else
                 <li>
-                    <a class="nav-link" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link" href="{{ route('login') }}">@lang('Login')</a>
                 </li>
                 <li>
-                    <a class="nav-link" href="{{ route('register') }}">Registriere</a>
+                    <a class="nav-link" href="{{ route('register') }}">@lang('Register')</a>
                 </li>
             @endif
         </ul>
