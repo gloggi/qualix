d7537cdda6865c88561bf03c56aeecc225a48899
Carlo Beltrame <carlo@beltra.me>
2019-11-19 11:29:32 +0100

    Fix typo and add documentation comment

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -9,11 +9,18 @@ use Illuminate\Support\Facades\Session;
 
 class Localization {
 
+    /**
+     * Set up the user interface language according to the visitor's preferences from session or browser settings.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
     public function handle(Request $request, Closure $next) {
         if (!Session::has('locale')) {
             Session::put('locale', $request->getPreferredLanguage(config('app.supported_locales')));
         }
-        App::setlocale(Session::get('locale'));
+        App::setLocale(Session::get('locale'));
         return $next($request);
     }
 
