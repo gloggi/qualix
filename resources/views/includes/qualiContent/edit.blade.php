4281d7509214f282cf999e09e69609b6cdaffda5
Carlo Beltrame <carlo@beltra.me>
2020-05-04 13:09:13 +0200

    Enable adding text blocks inside qualiRequirements

--- resources/views/includes/qualiContent/edit.blade.php
+++ resources/views/includes/qualiContent/edit.blade.php
@@ -19,19 +19,30 @@
         name="contents"
         :value="{{ old('contents') ?? json_encode($quali->contents) }}"
         :translations="{{ json_encode($translations) }}">
+
         <template v-slot:text="{ value, remove, translations }"><element-text v-model="value" :remove="remove" :translations="translations" /></template>
+
         <template v-slot:observation="{ value, remove, translations }"><element-observation v-model="value" :remove="remove" :translations="translations" /></template>
-        <template v-slot:requirement="{ value, translations }">
-            <element-requirement v-model="value" :translations="translations">
-                <list-builder :value="value.contents" :translations="translations">
-                    <template v-slot:text="{ value }"><element-text v-model="value" /></template>
-                    <template v-slot:observation="{ value, translations }"><element-observation v-model="value" :translations="translations" /></template>
-                </list-builder>
-            </element-requirement>
-        </template>
+
+        <template v-slot:requirement="{ value, translations }"><element-requirement v-model="value" :translations="translations">
+
+            <list-builder :value="value.contents" :translations="translations">
+
+                <template v-slot:text="{ value, remove, translations }"><element-text v-model="value" :remove="remove" :translations="translations" /></template>
+
+                <template v-slot:observation="{ value, remove, translations }"><element-observation v-model="value" :remove="remove" :translations="translations" /></template>
+
+                <template v-slot:add-text="{ addElement }">
+                    <button-add @click="addElement" :payload="{{ json_encode(['type' => 'text','content' => '', 'id' => null]) }}">{{__('t.views.quali_content.text_element')}}</button-add>
+                </template>
+
+            </list-builder>
+
+        </element-requirement></template>
 
         <template v-slot:add-text="{ addElement }">
             <button-add @click="addElement" :payload="{{ json_encode(['type' => 'text','content' => '', 'id' => null]) }}">{{__('t.views.quali_content.text_element')}}</button-add>
         </template>
+
     </list-builder>
 @endcomponent
