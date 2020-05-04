f16e7f244a5376da557b5c54be8b1599bae5e094
Carlo Beltrame <carlo@beltra.me>
2020-05-04 13:09:13 +0200

    Enable adding text blocks inside qualiRequirements

--- app/Http/Controllers/QualiContentController.php
+++ app/Http/Controllers/QualiContentController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\QualiContentRequest;
 use App\Models\Course;
 use App\Models\Participant;
 use App\Models\Quali;
+use App\Models\QualiRequirement;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
@@ -52,42 +53,8 @@ class QualiContentController extends Controller {
      */
     public function update(QualiContentRequest $request, Course $course, Participant $participant, Quali $quali) {
         $data = collect($request->validated()['contents']);
-        return DB::transaction(function() use($request, $course, $participant, $quali, $data) {
-
-            if ($correctedContents = $this->qualiRequirementsHaveBeenChanged($data, $quali)) {
-                // Edit the original request because old_input is derived from that
-                app(Request::class)->offsetSet('contents', $correctedContents->toJson());
-                throw ValidationException::withMessages(['contents' => trans('t.views.quali_content.error_requirements_changed')]);
-            }
-
-            $allNoteIds = $data->filter(function ($element) { return ($element['type'] === 'text') && $element['content']; })->map(function ($element) { return $element['id']; });
-            $quali->notes()->whereNotIn('id', $allNoteIds)->delete();
-
-            $allQualiObservationIds = $data->filter(function ($element) { return $element['type'] === 'observation'; })->map(function ($element) { return $element['id']; });
-            $quali->observations()->whereNotIn('id', $allQualiObservationIds)->detach();
-
-
-            $order = 0;
-
-            $data->each(function ($element) use($quali, &$order) {
-                switch ($element['type']) {
-                    case 'text':
-                        if ($element['content']) {
-                            $quali->notes()->updateOrCreate(['id' => $element['id']], ['order' => $order++, 'notes' => $element['content']]);
-                        }
-                        break;
-                    case 'observation':
-                        if ($element['id']) {
-                            $quali->observations()->syncWithoutDetaching([$element['id'] => ['order' => $order++]]);
-                        }
-                        break;
-                    case 'requirement':
-                        $quali->requirements()->find($element['id'])->update(['order' => $order++, 'passed' => $element['passed']]);
-                        // TODO reordering inside of a requirement
-                        break;
-                }
-            });
-
+        return DB::transaction(function() use($course, $participant, $quali, $data) {
+            $this->updateContents($quali, $data);
             return Redirect::route('qualiContent.detail', ['course' => $course->id, 'participant' => $participant->id, 'quali' => $quali->id]);
         });
     }
@@ -98,6 +65,59 @@ class QualiContentController extends Controller {
         })->all();
     }
 
+    /**
+     * @param Quali|QualiRequirement $parent
+     * @param Collection $data
+     * @throws ValidationException
+     */
+    protected function updateContents($parent, $data) {
+
+        if ($parent instanceof Quali) {
+            if ($correctedContents = $this->qualiRequirementsHaveBeenChanged($data, $parent)) {
+                // Edit the original request because old_input is derived from that
+                app(Request::class)->offsetSet('contents', $correctedContents->toJson());
+                throw ValidationException::withMessages(['contents' => trans('t.views.quali_content.error_requirements_changed')]);
+            }
+        } else {
+            $data = $data->reject(function ($element) { return $element['type'] === 'requirement'; });
+        }
+
+        $allNoteIds = $data->filter(function ($element) { return ($element['type'] === 'text') && $element['content']; })->map(function ($element) { return $element['id']; });
+        $parent->notes()->whereNotIn('id', $allNoteIds)->delete();
+
+        $allQualiObservationIds = $data->filter(function ($element) { return $element['type'] === 'observation'; })->map(function ($element) { return $element['id']; });
+        $parent->observations()->whereNotIn('id', $allQualiObservationIds)->detach();
+
+        $order = 0;
+        $data->each(function ($element) use($parent, &$order) {
+            switch ($element['type']) {
+                case 'text':
+                    if ($element['content']) {
+                        $parent->notes()->updateOrCreate(['id' => $element['id']], ['order' => $order++, 'notes' => $element['content']]);
+                    }
+                    break;
+                case 'observation':
+                    if ($element['id']) {
+                        $parent->observations()->syncWithoutDetaching([$element['id'] => ['order' => $order++]]);
+                    }
+                    break;
+                case 'requirement':
+                    if ($parent instanceof Quali) {
+                        /** @var QualiRequirement $requirement */
+                        $requirement = $parent->requirements()->find($element['id']);
+                        $requirement->update(['order' => $order++, 'passed' => $element['passed']]);
+                        $this->updateContents($requirement, collect($element['contents']));
+                    }
+                    break;
+            }
+        });
+    }
+
+    /**
+     * @param Collection $qualiContentsFromRequest
+     * @param $qualiFromDB
+     * @return bool|Collection
+     */
     protected function qualiRequirementsHaveBeenChanged(Collection $qualiContentsFromRequest, Quali $qualiFromDB) {
         $requestRequirementIds = $qualiContentsFromRequest
             ->filter(function ($element) { return $element['type'] === 'requirement'; })
