6ab8549e58e791cc5796506a27d70e38f6394378
Carlo Beltrame <carlo@beltra.me>
2020-05-04 12:29:25 +0200

    Cosmetic improvements

--- resources/js/Components/ElementRequirement.vue
+++ resources/js/Components/ElementRequirement.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="card-header d-flex p-2">
       <quali-requirement-menu v-model="value" class="mr-2 my-auto"></quali-requirement-menu>
-      <div class="flex-grow-1 my-auto">{{ value.content }}</div>
+      <h5 class="flex-grow-1 my-auto">{{ value.content }}</h5>
       <i class="fas fa-arrows-alt mx-2 handle my-auto"></i>
     </div>
     <div class="card-body">
