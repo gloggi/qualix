5e22b57ee6b4805f0e24388d402876a5689ea5c9
Carlo Beltrame <carlo@beltra.me>
2020-08-09 14:15:34 +0200

    Make form restoration work in 500 error page even when not logged in

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,7 @@ class Authenticate extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return string
      */
-    protected function redirectTo($request)
+    public function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
