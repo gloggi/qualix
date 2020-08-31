f15354e1a214fb3e92eddf516811b7428e90518e
Allan Benelli <allan.benelli@bluewin.ch>
2020-08-31 22:26:39 +0200

    Button Styling and adding GroupParticipant IDs

--- resources/views/includes/participant/groups.blade.php
+++ resources/views/includes/participant/groups.blade.php
@@ -1,8 +1,8 @@
-<div class="row my-3">
+<div class="row">
 
     <div class="col">
         @foreach ($groups as $group)
-            <a href="{{ route('observation.new', ['course' => $course->id, 'group' => $group->id]) }}" class="btn btn-secondary"><i class="fas fa-binoculars"></i> {{__($group->group_name)}}</a>
+            <a href="{{ route('observation.new', ['course' => $course->id, 'participant' => $group->participants->implode('id',',')]) }}" class="btn btn-secondary my-1"><i class="fas fa-binoculars"></i> {{__($group->group_name)}}</a>
         @endforeach
 
     </div>
