72368ce9ed111d2818498ce6fe9e28b63f21daea
Allan Benelli <allan.benelli@bluewin.ch>
2020-09-04 17:35:19 +0200

    prepare controller with index, edit & destroy

--- app/Http/Controllers/ObservationOrderController.php
+++ app/Http/Controllers/ObservationOrderController.php
@@ -2,30 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use App\ObservationOrder;
+use App\Models\Course;
+use App\Models\ObservationOrder;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\DB;
+
 
 class ObservationOrderController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        //
+        return view('admin.observationOrders.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -38,33 +34,23 @@ class ObservationOrderController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\ObservationOrder  $observationOrder
-     * @return \Illuminate\Http\Response
-     */
-    public function show(ObservationOrder $observationOrder)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\ObservationOrder  $observationOrder
+     * @param  \App\Models\ObservationOrder  $observationOrder
      * @return \Illuminate\Http\Response
      */
     public function edit(ObservationOrder $observationOrder)
     {
-        //
+        return view('admin.observationOrders.edit', ['observationOrder' => $observationOrder]);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\ObservationOrder  $observationOrder
+     * @param  \App\Models\ObservationOrder  $observationOrder
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, ObservationOrder $observationOrder)
@@ -75,11 +61,17 @@ class ObservationOrderController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\ObservationOrder  $observationOrder
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Course $course
+     * @param  ObservationOrder  $observationOrder
+     * @return RedirectResponse
+     *
      */
-    public function destroy(ObservationOrder $observationOrder)
+    public function destroy(Request $request, Course $course, ObservationOrder $observationOrder)
     {
-        //
+        $observationOrder->delete();
+        $request->session()->flash('alert-success', __('t.views.admin.observation_orders.delete_success'));
+        return Redirect::route('admin.observationOrders.index', ['course' => $course->id]);
+
     }
 }
--- app/Models/ObservationOrder.php
+++ app/Models/ObservationOrder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ObservationOrder extends Model
 {
+    protected $table = 'observation_orders';
 
     protected $fillable = ['user_id', 'block_id', 'participant_id'];
 
