35b3f661ae3d28c74f2bd92e93416f70dc987b21
Allan Benelli <allan.benelli@bluewin.ch>
2020-09-04 17:24:59 +0200

    migration: table and foreign keys

--- database/migrations/2020_09_04_145745_create_observation_orders_table.php
+++ database/migrations/2020_09_04_145745_create_observation_orders_table.php
@@ -15,7 +15,17 @@ class CreateObservationOrdersTable extends Migration
     {
         Schema::create('observation_orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id')->nullable(false);
+            $table->integer('block_id')->nullable(false);
+            $table->integer('participant_id')->nullable(false);
             $table->timestamps();
+        });
+        Schema::table('observation_orders', function(Blueprint $table)
+        {
+            $table->foreign('user_id', 'fk_user_observation_orders')->references('id')->on('users')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('block_id', 'fk_block_observation_orders')->references('id')->on('blocks')->onUpdate('CASCADE')->onDelete('CASCADE');
+            $table->foreign('participant_id', 'fk_participants_observation_orders')->references('id')->on('participants')->onUpdate('CASCADE')->onDelete('CASCADE');
+
         });
     }
 
@@ -26,6 +36,12 @@ class CreateObservationOrdersTable extends Migration
      */
     public function down()
     {
+        Schema::table('observation_orders', function(Blueprint $table)
+        {
+            $table->dropForeign('fk_user_observation_orders');
+            $table->dropForeign('fk_block_observation_orders');
+            $table->dropForeign('fk_participants_observation_orders');
+        });
         Schema::dropIfExists('observation_orders');
     }
 }
