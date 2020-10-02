a646315d012ee557de8300f80685e0773f35c42c
Carlo Beltrame <carlo@beltra.me>
2020-10-02 16:28:09 +0200

    Redesign a little with help from @manuelmeister

--- resources/views/crib.blade.php
+++ resources/views/crib.blade.php
@@ -38,9 +38,9 @@
                     <b-list-group flush>
                         @foreach ($day as $block)
                             <b-list-group-item class="mb-0 p-0">
-                                <b-list-group horizontal class="row  m-0">
-                                    <b-list-group-item tag="h5" class="border-0  mb-0 col-sm-12 col-md-6">
-                                        {{ $block->blockname_and_number }}
+                                <b-list-group horizontal class="row m-0">
+                                    <b-list-group-item :href="routeUri('observation.new', {course: {{ $course->id }}, block: {{ $block->id }} })" class="border-0  mb-0 col-sm-12 col-md-6">
+                                        <h5>{{ $block->blockname_and_number }}</h5>
                                         @if(count($block->mandatory_requirements))
                                             <br>
                                             {{__('t.views.crib.mandatory_requirements')}}:
@@ -56,17 +56,19 @@
                                             @endforeach
                                         @endif
                                     </b-list-group-item>
-                                    <b-list-group-item tag="h5" class="border-0 mb-0 col-sm-12 col-md-6">
+                                    <b-list-group-item class="border-0 mb-0 col-sm-12 col-md-6">
                                         @if(isset($trainerObservationAssignments[$block->id]))
                                             <div class="row">
                                                 @foreach($trainerObservationAssignments[$block->id] as $participant)
-                                                    <div class="col-6 col-sm-4 col-md-4 col-lg-3 mb-1 text-center">
+                                                    <div class="col-4 col-sm-3 col-md-4 col-lg-3 mb-1 text-center">
                                                         <a href="{{ route('observation.new', ['course' => $course->id, 'participant' => $participant->id, 'block' => $block->id, 'crib' => true]) }}">
-                                                            <div class="card rounded-circle mb-0 position-relative">
-                                                                <img class="card-img-top img img-responsive full-width" src="{{ $participant->image_url != null ? asset(Storage::url($participant->image_url)) : asset('images/was-gaffsch.svg') }}" alt="{{ $participant->scout_name }}">
+                                                            <div class="mb-0 position-relative">
+                                                                <img class="card-img-top rounded-circle img img-responsive full-width" src="{{ $participant->image_url != null ? asset(Storage::url($participant->image_url)) : asset('images/was-gaffsch.svg') }}" alt="{{ $participant->scout_name }}">
                                                                 <div class="card-img-overlay w-100 p-0 d-flex flex-column ">
-                                                                    <b-button variant="{{$participant->observation_count >= $neededObservations ? 'outline-success' : 'outline-danger'}}" class="bg-white py-0 mt-1 w-100 mt-auto text-overflow-ellipsis">{{$participant->scout_name}}</b-button>
+                                                                    <b-badge v-if="{{ $participant->observation_count }}" variant="primary" class="ml-auto mb-auto font-size-larger" pill>{{ $participant->observation_count }}</b-badge>
+                                                                    <b-badge v-else variant="danger" class="ml-auto mb-auto font-size-larger" pill>{{ $participant->observation_count }}</b-badge>
                                                                 </div>
+                                                                <p class="text-overflow-ellipsis">{{ $participant->scout_name }}</p>
                                                             </div>
 
                                                         </a>
