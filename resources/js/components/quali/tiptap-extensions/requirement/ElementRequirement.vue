33560031f5f45ea0cfe74fe59dcee56d0be2bfcf
Carlo Beltrame <carlo@beltra.me>
2020-10-04 11:12:10 +0200

    Only show observations button when there are actually observations to add

--- resources/js/components/quali/tiptap-extensions/requirement/ElementRequirement.vue
+++ resources/js/components/quali/tiptap-extensions/requirement/ElementRequirement.vue
@@ -4,7 +4,7 @@
     <h5 class="flex-grow-1 my-auto">{{ requirement.content | ucfirst }}</h5>
     <b-dropdown v-if="editor.options.editable" dropleft class="mr-2 requirement-menu" no-caret variant="link"><template v-slot:button-content>
         <i class="fas fa-ellipsis-v"></i>
-      </template><b-dropdown-item-button @click="onChange(1)"><i class="text-success fas fa-check-circle mr-3"></i> {{ $t('t.views.quali_content.requirements.passed') }}</b-dropdown-item-button><b-dropdown-item-button @click="onChange(null)"><i class="text-primary fas fa-binoculars mr-3"></i> {{ $t('t.views.quali_content.requirements.observing') }}</b-dropdown-item-button><b-dropdown-item-button @click="onChange(0)"><i class="text-danger fas fa-times-circle mr-3"></i> {{ $t('t.views.quali_content.requirements.failed') }}</b-dropdown-item-button><b-dropdown-item-button @click="selectObservation"><i class="text-primary fas fa-plus mr-3"></i> {{$t('t.models.observation.one')}}</b-dropdown-item-button></b-dropdown>
+      </template><b-dropdown-item-button @click="onChange(1)"><i class="text-success fas fa-check-circle mr-3"></i> {{ $t('t.views.quali_content.requirements.passed') }}</b-dropdown-item-button><b-dropdown-item-button @click="onChange(null)"><i class="text-primary fas fa-binoculars mr-3"></i> {{ $t('t.views.quali_content.requirements.observing') }}</b-dropdown-item-button><b-dropdown-item-button @click="onChange(0)"><i class="text-danger fas fa-times-circle mr-3"></i> {{ $t('t.views.quali_content.requirements.failed') }}</b-dropdown-item-button><b-dropdown-item-button v-if="observations.length" @click="selectObservation"><i class="text-primary fas fa-plus mr-3"></i> {{$t('t.models.observation.one')}}</b-dropdown-item-button></b-dropdown>
   </div>
 </template>
 
@@ -23,7 +23,7 @@ export default {
   // `getPos` is a function to retrieve the start position of the node
   // `decorations` is an array of decorations around the node
   props: [ 'node', 'updateAttrs', 'view', 'options', 'selected', 'editor', 'getPos', 'decorations'],
-  inject: [ 'requirements', 'addObservation', 'courseId' ],
+  inject: [ 'requirements', 'addObservation', 'observations', 'courseId' ],
   computed: {
     requirement() {
       const requirement = this.requirements.find(requirement => requirement.id === this.node.attrs.id)
