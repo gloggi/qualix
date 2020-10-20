e5763da7881bcf085b8d290e37c666d772ffd4b0
Carlo Beltrame <carlo@beltra.me>
2020-10-20 11:33:29 +0200

    Fix an edge case of ucfirst filter

--- resources/js/components/quali/tiptap-extensions/requirement/ElementRequirement.vue
+++ resources/js/components/quali/tiptap-extensions/requirement/ElementRequirement.vue
@@ -35,6 +35,7 @@ export default {
   },
   filters: {
     ucfirst(text) {
+      if (!text || typeof text !== 'string') return text
       return text.charAt(0).toUpperCase() + text.slice(1)
     }
   },
