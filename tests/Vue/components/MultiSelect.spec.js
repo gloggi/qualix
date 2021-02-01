1894933fd1c44b45ef9bfb6d18af2be4bd7f9f2a
Carlo Beltrame <carlo@beltra.me>
2021-02-01 21:20:55 +0100

    Add tests for the clear button in a multiselect

--- tests/Vue/components/MultiSelect.spec.js
+++ tests/Vue/components/MultiSelect.spec.js
@@ -50,99 +50,76 @@ it('should open when clicked', async () => {
   })
 })
 
-it('should display no selected option initially', async () => {
-  const multiSelect = render(MultiSelect, {
-    props: {
-      options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
-    },
-    mocks: { '$t': () => {} }
-  })
-
-  expect(multiSelect.container).not.toHaveVisibleTextContent()
-})
-
-it('should display the placeholder initially', async () => {
-  const multiSelect = render(MultiSelect, {
-    props: {
-      options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
-      placeholder: 'Select one',
-    },
-    mocks: { '$t': () => {} }
-  })
-
-  expect(multiSelect.container).toHaveVisibleTextContent('Select one')
-})
-
-it('should display the initial value', async () => {
-  const multiSelect = render(MultiSelect, {
-    props: {
-      options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
-      value: '2'
-    },
-    mocks: { '$t': () => {} }
-  })
-
-  expect(multiSelect.container).toHaveVisibleTextContent('Bar')
-})
-
-it('should prefer to display the initial value over the placeholder', async () => {
-  const multiSelect = render(MultiSelect, {
-    props: {
-      options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
-      placeholder: 'Select one',
-      value: '2'
-    },
-    mocks: { '$t': () => {} }
-  })
-
-  expect(multiSelect.container).toHaveVisibleTextContent('Bar')
-})
-
-it('should display the selected option', async () => {
-  const multiSelect = render(MultiSelect, {
-    props: {
-      options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
-    },
-    mocks: { '$t': () => {} }
-  })
-
-  userEvent.click(screen.getByRole('combobox'))
-
-  await waitFor(() => {
-    expect(screen.getByText('Foo')).toBeVisible()
-    expect(screen.getByText('Bar')).toBeVisible()
-  })
-
-  userEvent.click(screen.getByText('Bar'))
-
-  await waitFor(() => {
-    expect(screen.getByText('Foo')).not.toBeVisible()
-  })
-
-  expect(multiSelect.container).toHaveVisibleTextContent('Bar')
-})
-
-describe('with name prop', () => {
+describe('main display', () => {
   it('should display no selected option initially', async () => {
     const multiSelect = render(MultiSelect, {
       props: {
         options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
-        name: 'my-input',
       },
-      mocks: { '$t': () => {} }
+      mocks: {
+        '$t': () => {
+        }
+      }
     })
 
     expect(multiSelect.container).not.toHaveVisibleTextContent()
-    expect(screen.getByTestId('formValue')).toHaveValue('')
+  })
+
+  it('should display the placeholder initially', async () => {
+    const multiSelect = render(MultiSelect, {
+      props: {
+        options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
+        placeholder: 'Select one',
+      },
+      mocks: {
+        '$t': () => {
+        }
+      }
+    })
+
+    expect(multiSelect.container).toHaveVisibleTextContent('Select one')
+  })
+
+  it('should display the initial value', async () => {
+    const multiSelect = render(MultiSelect, {
+      props: {
+        options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
+        value: '2'
+      },
+      mocks: {
+        '$t': () => {
+        }
+      }
+    })
+
+    expect(multiSelect.container).toHaveVisibleTextContent('Bar')
+  })
+
+  it('should prefer to display the initial value over the placeholder', async () => {
+    const multiSelect = render(MultiSelect, {
+      props: {
+        options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
+        placeholder: 'Select one',
+        value: '2'
+      },
+      mocks: {
+        '$t': () => {
+        }
+      }
+    })
+
+    expect(multiSelect.container).toHaveVisibleTextContent('Bar')
   })
 
   it('should display the selected option', async () => {
     const multiSelect = render(MultiSelect, {
       props: {
         options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
-        name: 'my-input',
       },
-      mocks: { '$t': () => {} }
+      mocks: {
+        '$t': () => {
+        }
+      }
     })
 
     userEvent.click(screen.getByRole('combobox'))
@@ -159,6 +136,154 @@ describe('with name prop', () => {
     })
 
     expect(multiSelect.container).toHaveVisibleTextContent('Bar')
-    expect(screen.getByTestId('formValue')).toHaveValue('2')
+  })
+
+  describe('with name prop', () => {
+    it('should display no selected option initially', async () => {
+      const multiSelect = render(MultiSelect, {
+        props: {
+          options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
+          name: 'my-input',
+        },
+        mocks: { '$t': () => {} }
+      })
+
+      expect(multiSelect.container).not.toHaveVisibleTextContent()
+      expect(screen.getByTestId('formValue')).toHaveValue('')
+    })
+
+    it('should display the initial value', async () => {
+      const multiSelect = render(MultiSelect, {
+        props: {
+          options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+          name: 'my-input',
+          value: '2'
+        },
+        mocks: { '$t': () => {} }
+      })
+
+      expect(multiSelect.container).toHaveVisibleTextContent('Bar')
+      expect(screen.getByTestId('formValue')).toHaveValue('2')
+    })
+
+    it('should display the selected option', async () => {
+      const multiSelect = render(MultiSelect, {
+        props: {
+          options: [{id: 1, label: 'Foo'}, {id: 2, label: 'Bar'}],
+          name: 'my-input',
+        },
+        mocks: { '$t': () => {} }
+      })
+
+      userEvent.click(screen.getByRole('combobox'))
+
+      await waitFor(() => {
+        expect(screen.getByText('Foo')).toBeVisible()
+        expect(screen.getByText('Bar')).toBeVisible()
+      })
+
+      userEvent.click(screen.getByText('Bar'))
+
+      await waitFor(() => {
+        expect(screen.getByText('Foo')).not.toBeVisible()
+      })
+
+      expect(multiSelect.container).toHaveVisibleTextContent('Bar')
+      expect(screen.getByTestId('formValue')).toHaveValue('2')
+    })
+  })
+})
+
+describe('clear button', () => {
+  it('should not display the clear button when show-clear is unset and no option is selected', async () => {
+    render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    expect(document.querySelector('.multiselect__clear')).not.toBeInTheDocument()
+  })
+
+  it('should not display the clear button when show-clear is false and no option is selected', async () => {
+    render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+        'show-clear': false,
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    expect(document.querySelector('.multiselect__clear')).not.toBeInTheDocument()
+  })
+
+  it('should not display the clear button when show-clear is true and no option is selected', async () => {
+    render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+        'show-clear': true,
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    expect(document.querySelector('.multiselect__clear')).not.toBeInTheDocument()
+  })
+
+  it('should not display the clear button when show-clear is unset and an option is selected', async () => {
+    render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+        value: '2',
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    expect(document.querySelector('.multiselect__clear')).not.toBeInTheDocument()
+  })
+
+  it('should not display the clear button when show-clear is false and an option is selected', async () => {
+    render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+        'show-clear': false,
+        value: '2',
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    expect(document.querySelector('.multiselect__clear')).not.toBeInTheDocument()
+  })
+
+  it('should display the clear button when show-clear is true and an option is selected', async () => {
+    render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+        'show-clear': true,
+        value: '2',
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    expect(document.querySelector('.multiselect__clear')).toBeInTheDocument()
+  })
+
+  it('should remove the selected entry when clicking the clear button', async () => {
+    const multiSelect = render(MultiSelect, {
+      props: {
+        options: [{ id: 1, label: 'Foo'}, { id: 2, label: 'Bar' }],
+        'show-clear': true,
+        value: '2',
+        name: 'my-input',
+      },
+      mocks: { '$t': () => {} }
+    })
+
+    userEvent.click(document.querySelector('.multiselect__clear'))
+
+    await waitFor(() => {
+      expect(multiSelect.container).not.toHaveVisibleTextContent()
+      expect(screen.getByTestId('formValue')).toHaveValue('')
+    })
   })
 })
