d3de1b525a4464178eebc2e875f0227d2bdf37b8
Carlo Beltrame <carlo@beltra.me>
2021-02-10 00:47:21 +0100

    Refactor CourseFactory to use more Laravel paradigms

--- app/Console/Commands/E2EScenario.php
+++ app/Console/Commands/E2EScenario.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Block;
 use App\Models\Course;
+use App\Models\Observation;
 use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\App;
@@ -53,7 +55,20 @@ class E2EScenario extends Command
         }
 
         /** @var Course $course */
-        $course = Course::factory()->create();
+        $course = Course::factory()
+            ->hasUsers(3)
+            ->hasRequirements(4)
+            ->hasParticipants(10)
+            ->has(Block::factory()
+                ->count(10)
+                ->has(Observation::factory()
+                    ->count(5)
+                    ->fromRandomUser()
+                    ->withRequirements()
+                    ->maybeMultiParticipant()
+                )
+            )
+            ->create();
 
         $course->users()->attach($userId);
 
