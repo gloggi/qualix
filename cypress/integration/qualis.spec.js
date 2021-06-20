a6e218546eeb17b4939c5ab0c3b5953fb9ad7f5c
Carlo Beltrame <carlo@beltra.me>
2021-06-20 17:24:48 +0200

    Add e2e test for the quali editor

---
+++ cypress/integration/qualis.spec.js
@@ -0,0 +1,41 @@
+import { useDatabaseResets } from "../support/databaseTransactions"
+
+describe('quali editor', () => {
+  useDatabaseResets()
+
+  let courseId
+
+  beforeEach(() => {
+    cy.then(() => {
+      cy.login().then(user => {
+        cy.artisan('e2e:scenario', { '--user-id': user.id })
+      })
+    })
+    cy.courseId().then(id => courseId = id)
+  })
+
+  it('creates a quali', () => {
+    cy.visit(`/course/${courseId}/admin/qualis`)
+
+    cy.get('[autofocus]').type('End-to-end test quali{enter}')
+
+    cy.contains('Das Quali "End-to-end test quali" wurde erfolgreich erstellt.')
+  })
+
+  it('edits a quali', () => {
+    cy.visit(`/course/${courseId}/participants`)
+    cy.get('img.card-img-top').first().click()
+    cy.get('td[data-label=Titel] a').first().click()
+    cy.contains('Quali Details')
+
+    cy.get('div.editor.form-control [contenteditable]').first().type("Text from end-to-end test\n")
+    cy.get('.btn-primary').click()
+
+    cy.get('[href$="/print"]')
+      .then(link => {
+        cy.visit(link.prop('href'))
+      });
+
+    cy.contains('Text from end-to-end test')
+  })
+})
