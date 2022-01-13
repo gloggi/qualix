916b17ff8bea44b4d6806a6de21ac6b8143449db
Carlo Beltrame <carlo@beltra.me>
2022-01-13 16:16:29 +0100

    Refactor test to more idiomatic PHPUnit

--- tests/Unit/Services/DateCalculatorTest.php
+++ tests/Unit/Services/DateCalculatorTest.php
@@ -6,25 +6,28 @@ use App\Services\DateCalculator;
 use Tests\TestCase;
 
 class DateCalculatorTest extends TestCase {
-
-    public function test_calculateYearFromWeekdayAndDate() {
+    /**
+     * @dataProvider getExamples
+     * @param $expected int expected outcome of the date calculation
+     * @param $parameters array input for the date calculator
+     */
+    public function test_calculateYearFromWeekdayAndDate($expected, $parameters) {
         // given
-        $examples = [
-            2019 => [2019, 5, 1, 12], // basic test (Sat, 12th of January 2019)
-            2020 => [2015, 6, 1, 12], // later year (Sun, 12th of January 2020)
-            2030 => [2020, 5, 1, 12], // long skip due to leap years (Sat, 1st of January 2030)
-            2016 => [2013, 0, 2, 29], // 29th of February (Mon, 29th of February 2016)
-        ];
         $dateCalculator = new DateCalculator;
 
-        foreach ($examples as $expected => $parameters) {
+        // when
+        $result = $dateCalculator->calculateYearFromWeekdayAndDate(...$parameters);
 
-            // when
-            $result = $dateCalculator->calculateYearFromWeekdayAndDate(...$parameters);
+        // then
+        $this->assertEquals($expected, $result);
+    }
 
-            // then
-            $this->assertEquals($expected, $result);
-
-        }
+    public function getExamples() {
+        return [
+            'basic test (Sat, 12th of January 2019)' => [2019, [2019, 5, 1, 12]],
+            'later year (Sun, 12th of January 2020)' => [2020, [2015, 6, 1, 12]],
+            'long skip due to leap years (Sat, 1st of January 2030)' => [2030, [2020, 5, 1, 12]],
+            '29th of February (Mon, 29th of February 2016)' => [2016, [2013, 0, 2, 29]],
+        ];
     }
 }
