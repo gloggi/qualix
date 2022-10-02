0b5624673691ddc0fa765c7e9fe110b4434a42a6
Carlo Beltrame <carlo@beltra.me>
2022-10-02 16:52:17 +0200

    Fix config fallback

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="page-footer text-center">
     <p class="footer-company-name">{{__('t.footer.slogan')}}
         @if(config('app.contact.link') !== null && config('app.contact.link') !== "")
-        - <a href="{{config('app.contact.link')}}">{{config('app.contact.text', __('t.footer.contact_text'))}}</a>
+        - <a href="{{config('app.contact.link')}}">{{config('app.contact.text') ?? __('t.footer.contact_text')}}</a>
         @endif
     </p>
 </footer>
