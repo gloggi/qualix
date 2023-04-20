b22ce61602cf8006f9d18534dcdbf6c5f7ba64d6
Carlo Beltrame <carlo@beltra.me>
2023-04-20 08:32:23 +0200

    Fix tests

--- cypress/e2e/requirements-matrix.cy.js
+++ cypress/e2e/requirements-matrix.cy.js
@@ -9,32 +9,66 @@ describe('requirements matrix', () => {
         cy.artisan('e2e:scenario', { '--user-id': user.id })
       })
     })
-    cy.courseId().then((courseId) => {
-      cy.create('App\\Models\\RequirementStatus', 1, { course_id: courseId, name: 'E2E status', color: 'green', icon: 'book' })
-
-      cy.visit(`/course/${courseId}/feedbacks`)
-      cy.contains('Anforderungs-Matrix')
-      cy.get('.card-header > [role="button"]').then(($el) => {
-        return $el.get(0).innerHTML
-      }).as('feedbackName')
-    })
   })
 
   it('can be displayed and edited', function () {
-    cy.contains('Anforderungs-Matrix').click()
-    cy.contains(`Anforderungs-Matrix ${this.feedbackName}`)
-    cy.get('[data-label]').eq(1).as('cell')
+    cy.courseId().then((courseId) => {
+      cy.create('App\\Models\\RequirementStatus', 1, { course_id: courseId, name: 'E2E status', color: 'green', icon: 'book' })
 
-    cy.get('@cell').click()
-    cy.get('#requirement-status').click()
-    cy.get('#requirement-status .multiselect__option').eq(3).click()
-    cy.get('#comment').type('{selectall}Test E2E Notes which should be truncated because they are way too long to fit into the matrix cell')
-    cy.contains('Speichern...')
-    cy.contains('Automatisch gespeichert')
+      cy.php(`App\\Models\\FeedbackData::orderBy('id', 'desc')->first();`).then(feedback => {
+        cy.visit(`/course/${courseId}`)
+        cy.contains(feedback.name).click()
 
-    cy.get('[aria-label="Close"]').click()
-    cy.contains('Automatisch gespeichert').should('not.exist')
-    cy.contains('Test E2E Notes which should be truncated because…')
-    cy.get('@cell').should('have.class', 'bg-green')
+        cy.contains('Anforderungs-Matrix')
+
+        cy.contains(`Anforderungs-Matrix ${feedback.name}`)
+        cy.get('[data-label]').eq(1).as('cell')
+
+        cy.get('@cell').click()
+        cy.get('#requirement-status').click()
+        cy.get('#requirement-status .multiselect__option').eq(3).click()
+        cy.get('#comment').type('{selectall}Test E2E Notes which should be truncated because they are way too long to fit into the matrix cell')
+        cy.contains('Speichern...')
+        cy.contains('Automatisch gespeichert')
+
+        cy.get('[aria-label="Close"]').click()
+        cy.contains('Automatisch gespeichert').should('not.exist')
+        cy.contains('Test E2E Notes which should be truncated because…')
+        cy.get('@cell').should('have.class', 'bg-green')
+      })
+
+    })
+  })
+
+  it('can be displayed and edited if there are multiple feedbacks in course', function () {
+    cy.courseId().then((courseId) => {
+      cy.create('App\\Models\\RequirementStatus', 1, { course_id: courseId, name: 'E2E status', color: 'green', icon: 'book' })
+      cy.create('App\\Models\\FeedbackData', 1, { course_id: courseId, name: 'E2E Feedback' })
+
+      cy.php(`App\\Models\\FeedbackData::orderBy('id', 'desc')->offset(1)->first();`).then(feedback => {
+        cy.visit(`/course/${courseId}`)
+        cy.contains('Rückmeldungen').click()
+        cy.contains('E2E Feedback')
+        cy.contains(feedback.name).click()
+
+        cy.contains('Anforderungs-Matrix')
+
+        cy.contains(`Anforderungs-Matrix ${feedback.name}`)
+        cy.get('[data-label]').eq(1).as('cell')
+
+        cy.get('@cell').click()
+        cy.get('#requirement-status').click()
+        cy.get('#requirement-status .multiselect__option').eq(3).click()
+        cy.get('#comment').type('{selectall}Test E2E Notes which should be truncated because they are way too long to fit into the matrix cell')
+        cy.contains('Speichern...')
+        cy.contains('Automatisch gespeichert')
+
+        cy.get('[aria-label="Close"]').click()
+        cy.contains('Automatisch gespeichert').should('not.exist')
+        cy.contains('Test E2E Notes which should be truncated because…')
+        cy.get('@cell').should('have.class', 'bg-green')
+      })
+
+    })
   })
 })
