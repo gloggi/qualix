28a6e6332ea7a63eb15a68769fcccddea2dc4842
Allan Benelli <allan.benelli@bluewin.ch>
2025-07-26 20:31:37 +0200

    fix(#260): use cols and rows instead of table

--- resources/js/components/feedback/allocation/FormFeedbackAllocation.vue
+++ resources/js/components/feedback/allocation/FormFeedbackAllocation.vue
@@ -1,106 +1,111 @@
 <template>
   <div>
     <form-basic :action="action">
+
       <b-card class="mb-4">
-        <b-row>
-          <b-col cols="12" md="6">
-            <h2 class="h5 mb-2">{{ $t('t.views.admin.feedbacks.allocation.trainer_settings') }}</h2>
+        <h2 class="h5 mb-2">
+          {{ $t('t.views.admin.feedbacks.allocation.trainer_settings') }}
+        </h2>
+
+        <b-row class="font-weight-bold border-bottom py-2">
+          <b-col cols="12" md="4">
+            {{ $t('t.views.admin.feedbacks.allocation.trainer') }}
+          </b-col>
+          <b-col cols="12" md="4">
+            {{ $t('t.views.admin.feedbacks.allocation.number_of_feedbacks_per_trainer') }}
+          </b-col>
+          <b-col cols="12" md="4">
+            {{ $t('t.views.admin.feedbacks.allocation.nogo_header') }}
           </b-col>
         </b-row>
-        <div>
-          <b-table-simple responsive small striped>
-            <b-thead>
-              <b-tr>
-                <b-th>{{ $t('t.views.admin.feedbacks.allocation.trainer') }}</b-th>
-                <b-th>{{ $t('t.views.admin.feedbacks.allocation.number_of_feedbacks_per_trainer') }}</b-th>
-                <b-th>{{ $t('t.views.admin.feedbacks.allocation.nogo_header') }}</b-th>
-              </b-tr>
-            </b-thead>
-            <b-tbody>
-              <b-tr v-for="trainer in trainerPreferences" :key="trainer.id">
-                <b-td>{{ trainer.name }}</b-td>
-                <b-td>
-                  <b-form-input
-                    :id="`trainerCapacity-${trainer.id}`"
-                    v-model="trainer.maxCapacity"
-                    :max="participantCount"
-                    :min="0"
-                    :name="`trainerCapacity[${trainer.id}]`"
-                    type="number"
-                  />
-                </b-td>
-                <b-td>
-                  <multi-select
-                    :id="`trainerNogos-${trainer.id}`"
-                    v-model="trainer.nogos"
-                    :options="participants"
-                    :placeholder="$t('t.views.admin.feedbacks.allocation.nogo')"
-                    display-field="scout_name"
-                    multiple
-                    track-by="id"
-                    valueField="id"
-                  />
-                </b-td>
-              </b-tr>
-            </b-tbody>
-          </b-table-simple>
-        </div>
+
+        <b-row
+          v-for="trainer in trainerPreferences"
+          :key="trainer.id"
+          class="align-items-center py-2 border-bottom"
+        >
+          <b-col class="align-middle" cols="12" md="4">
+            {{ trainer.name }}
+          </b-col>
+          <b-col cols="12" md="4">
+            <b-form-input
+              :id="`trainerCapacity-${trainer.id}`"
+              v-model="trainer.maxCapacity"
+              :max="participantCount"
+              :min="0"
+              :name="`trainerCapacity[${trainer.id}]`"
+              type="number"
+            />
+          </b-col>
+          <b-col cols="12" md="4">
+            <multi-select
+              :id="`trainerNogos-${trainer.id}`"
+              v-model="trainer.nogos"
+              :options="participants"
+              :placeholder="$t('t.views.admin.feedbacks.allocation.nogo')"
+              display-field="scout_name"
+              multiple
+              track-by="id"
+              valueField="id"
+            />
+          </b-col>
+        </b-row>
       </b-card>
 
       <b-card class="mb-4">
-        <b-row>
-          <b-col cols="12" md="6">
-            <h2 class="h5 mb-2">{{ $t('t.views.admin.feedbacks.allocation.participant_preferences') }}</h2>
+        <h2 class="h5 mb-2">{{ $t('t.views.admin.feedbacks.allocation.participant_preferences') }}</h2>
+
+        <b-row class="font-weight-bold border-bottom py-2">
+          <b-col cols="12" md="4">
+            {{ $t('t.views.admin.feedbacks.allocation.participant') }}
+          </b-col>
+          <b-col cols="12" md="4">
+            {{ $t('t.views.admin.feedbacks.allocation.wishes') }}
+          </b-col>
+          <b-col cols="12" md="4">
+            {{ $t('t.views.admin.feedbacks.allocation.nogo_header') }}
           </b-col>
         </b-row>
 
-        <b-table-simple responsive small striped>
-          <b-thead>
-            <b-tr>
-              <b-th>{{ $t('t.views.admin.feedbacks.allocation.participant') }}</b-th>
-              <b-th>
-                {{ $t('t.views.admin.feedbacks.allocation.wishes') }}
-              </b-th>
-              <b-th>{{ $t('t.views.admin.feedbacks.allocation.nogo_header') }}</b-th>
-            </b-tr>
-          </b-thead>
-          <b-tbody>
-            <b-tr v-for="participant in participantPreferences" :key="`participant-${participant.id}`">
-              <b-td>{{ participant.name }}</b-td>
-
-              <b-td>
-                <multi-select
-                  :id="`participantPreference-${participant.id}`"
-                  v-model="participant.preferences"
-                  multiple
-                  :options="trainersWithPrio[participant.id]"
-                  :placeholder="$t('t.views.admin.feedbacks.allocation.trainer')"
-                  :show-clear="true"
-                  display-field="name"
-                  track-by="id"
-                />
-              </b-td>
-
-              <b-td>
-                <multi-select
-                  :id="`participantNogos-${participant.id}`"
-                  v-model="participant.forbidden"
-                  :options="trainers"
-                  :placeholder="$t('t.views.admin.feedbacks.allocation.nogo')"
-                  display-field="name"
-                  multiple
-                  track-by="id"
-                />
-              </b-td>
-            </b-tr>
-          </b-tbody>
-        </b-table-simple>
+        <b-row
+          v-for="participant in participantPreferences"
+          :key="`participant-${participant.id}`"
+          class="align-items-center py-2 border-bottom"
+        >
+          <b-col cols="12" md="4">
+            {{ participant.name }}
+          </b-col>
+          <b-col cols="12" md="4">
+            <multi-select
+              :id="`participantPreference-${participant.id}`"
+              v-model="participant.preferences"
+              :options="trainersWithPrio[participant.id]"
+              :placeholder="$t('t.views.admin.feedbacks.allocation.trainer')"
+              :show-clear="true"
+              display-field="name"
+              multiple
+              track-by="id"
+            />
+          </b-col>
+          <b-col cols="12" md="4">
+            <multi-select
+              :id="`participantNogos-${participant.id}`"
+              v-model="participant.forbidden"
+              :options="trainers"
+              :placeholder="$t('t.views.admin.feedbacks.allocation.nogo')"
+              display-field="name"
+              multiple
+              track-by="id"
+            />
+          </b-col>
+        </b-row>
       </b-card>
 
       <b-row class="align-items-center mb-4">
         <b-col md="6">
           <label class="form-label mb-1" for="priority-slider">
-            {{ $t('t.views.admin.feedbacks.allocation.prioritization_weight') }}</label>
+            {{ $t('t.views.admin.feedbacks.allocation.prioritization_weight') }}
+          </label>
           <b-form-input
             id="priority-slider"
             v-model="defaultPriorityIndex"
@@ -117,78 +122,73 @@
           <help-text
             id="prioritizationWeightExplanation"
             :params="{
-            heavy: $t('t.views.admin.feedbacks.allocation.prioritization_weights.heavy'),
-            low: $t('t.views.admin.feedbacks.allocation.prioritization_weights.low'),
-            heavy_two: $t('t.views.admin.feedbacks.allocation.prioritization_weights.heavy')
-          }"
+              heavy: $t('t.views.admin.feedbacks.allocation.prioritization_weights.heavy'),
+              low: $t('t.views.admin.feedbacks.allocation.prioritization_weights.low'),
+              heavy_two: $t('t.views.admin.feedbacks.allocation.prioritization_weights.heavy')
+            }"
             trans="t.views.admin.feedbacks.allocation.prioritization_weight_help"
           />
         </b-col>
+
         <b-col class="text-right" md="6">
-          <b-button
-            class="mr-2"
-            variant="primary"
-            @click.prevent="submitForm(false)"
-          >
+          <b-button class="mr-2" variant="primary" @click.prevent="submitForm(false)">
             <i class="fas fa-magic mr-1"></i>
             {{ $t('t.views.admin.feedbacks.allocation.generate_allocation') }}
           </b-button>
 
-          <b-button
-            v-if="mappedAllocations.length"
-            variant="outline-secondary"
-            @click.prevent="submitForm(true)"
-          >
+          <b-button v-if="mappedAllocations.length" variant="outline-secondary" @click.prevent="submitForm(true)">
             <i class="fas fa-random mr-1"></i>
             {{ $t('t.views.admin.feedbacks.allocation.regenerate_allocation') }}
           </b-button>
         </b-col>
-
       </b-row>
     </form-basic>
     <b-card v-if="mappedAllocations.length">
       <form-basic :action="updateAssignmentAction">
+        <h2 class="h5 mb-3">{{ $t('t.views.admin.feedbacks.allocation.allocations') }}</h2>
 
-        <h2 class="h5">{{ $t('t.views.admin.feedbacks.allocation.allocations') }}</h2>
-        <b-table-simple responsive small striped>
-          <b-thead>
-            <b-tr>
-              <b-th>{{ $t('t.views.admin.feedbacks.allocation.participant') }}</b-th>
-              <b-th>{{ $t('t.views.admin.feedbacks.allocation.trainer') }}</b-th>
-            </b-tr>
-          </b-thead>
-          <b-tbody>
+        <b-row class="font-weight-bold border-bottom py-2">
+          <b-col cols="12" md="6">
+            {{ $t('t.views.admin.feedbacks.allocation.participant') }}
+          </b-col>
+          <b-col cols="12" md="6">
+            {{ $t('t.views.admin.feedbacks.allocation.trainer') }}
+          </b-col>
+        </b-row>
 
-            <b-tr v-for="(assignment, index) in mappedAllocations" :key="index">
-              <b-td>{{ assignment.participantName }}</b-td>
-              <b-td>
-                <multi-select
-                  :id="`allocation-${index}`"
-                  :key="assignment.participantId"
-                  v-model="assignment.trainerId"
-                  :multiple="true"
-                  :name="`feedbacks[${assignment.participantId}][users]`"
-                  :options="trainers"
-                  :show-clear="true"
-                  display-field="name"
-                />
-              </b-td>
-            </b-tr>
-          </b-tbody>
-        </b-table-simple>
+        <b-row
+          v-for="(assignment, index) in mappedAllocations"
+          :key="index"
+          class="align-items-center py-2 border-bottom"
+        >
+          <b-col cols="12" md="6">
+            {{ assignment.participantName }}
+          </b-col>
+          <b-col cols="12" md="6">
+            <multi-select
+              :id="`allocation-${index}`"
+              :key="assignment.participantId"
+              v-model="assignment.trainerId"
+              :multiple="true"
+              :name="`feedbacks[${assignment.participantId}][users]`"
+              :options="trainers"
+              :show-clear="true"
+              display-field="name"
+            />
+          </b-col>
+        </b-row>
+
         <b-button
-          class="float-right"
+          class="float-right mt-3"
           type="submit"
           variant="primary"
         >
           {{ $t('t.views.admin.feedbacks.allocation.confirm_allocation') }}
         </b-button>
       </form-basic>
-
-
     </b-card>
-  </div>
 
+  </div>
 </template>
 
 <script>
@@ -330,4 +330,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
